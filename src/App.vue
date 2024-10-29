2c92dbe0ab7818f4726ba972aa2eaead95ead5d5
GeJiaXiang <353358601@qq.com>
2024-10-29 14:57:03 +0800

    添加说明

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ console.log("当前黑暗模式：" + isDark.value)
   <div class="container">
     <header>
       <h1>FlowGuide</h1>
-      <SwitchIcon darkBackground="#fff" lightBackground="#2D2D2D" />
+      <SwitchIcon />
     </header>
     <SiteGroup :urls="urls"></SiteGroup>
     <article style="margin-top: 20px;">
@@ -25,7 +25,7 @@ console.log("当前黑暗模式：" + isDark.value)
       <h2>说明</h2>
       <p>✅原理：在浏览器中通过http请求尝试每一个预配置的地址，可以得知每个地址的连通性情况，然后你可以选择一个地址进行访问</p>
       <p>✅状态：checking-正在检查，success-成功，failed-失败</p>
-      <p>❎自动跳转：使用参数`?auto={name}`将会自动选择最快的地址跳转，未知的标记不会做任何动作</p>
+      <p>❎自动跳转：使用参数`?auto={name}`将会自动选择最快的地址跳转，未知的标记不会做任何动作；你可以将自动跳转的url加入收藏夹，这样就可以实现静默自动化选择访问线路</p>
     </article>
     <footer>
       <p>本页面使用vue3构建，托管于阿里云OSS，仅作为学习调试用途。</p>
